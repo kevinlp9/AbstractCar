a471f659742f8d860c9b2318226b8142438d8627
kevinlp9 <atilano.kevin.h17@gmail.com>
2025-09-03 11:11:17 -0600

    3/9/25

--- src/Classes/Camion.java
+++ src/Classes/Camion.java
@@ -1,4 +1,13 @@
 package Classes;
 
-public class Camion {
+public class Camion extends Vehiculos{
+
+    public Camion(String marca, String modelo, int km) {
+        super(marca, modelo, km);
+    }
+
+    @Override
+    public double calcularMantenimiento() {
+        return 1500+ km*.30;
+    }
 }
